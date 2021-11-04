3b454cb6276183a86c18dda9c8e3749c4c50c23c
DiceNameIsMy <dicenameismy@gmail.com>
2021-11-04 13:24:23 +0600

    add swagger docs to project

--- project/config/urls.py
+++ project/config/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
+
+schema_view = get_schema_view(
+    title='Users API', 
+    renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer]
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include([
@@ -9,4 +17,5 @@ urlpatterns = [
             path('surveys/', include('apps.surveys.api.v1.urls')),
         ])),
     ])),
+    path('docs/', schema_view, name='docs'),
 ]
